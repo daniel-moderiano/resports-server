13be107a0da1de5a737d5c9ccc6577c65b8d204f
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-05-31 10:31:22 +0930

    Add Jest/TS support

---
+++ src/__tests__/app.test.ts
@@ -0,0 +1,19 @@
+import app from "../app";
+import request from 'supertest'
+
+// Format #1
+it("Returns hello on GET route", done => {
+  request(app)
+    .get('/')
+    .expect('Hello')
+    .expect(200, done)
+});
+
+// #Format #2
+it("Return Hello once more", async () => {
+  const res = await request(app).get('/');
+  expect(res.statusCode).toEqual(200);
+  expect(res.text).toBe('Hello');
+});
+
+
