ac8cf6166c5d4fc3b12493f5bbf546d803782729
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-06-09 21:37:00 +0930

    Add OKTA example files for session auth middleware and a few different routes

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,26 @@ process.env.TEST_ENV = 'false';
 
 const app: Application = express();
 
+const oidc = app.locals.oidc;
+
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello')
 });
 
-// (async () => {
-//   try {
-//     await getDb().connect();
-//     console.log('DB connected')
-//   } catch (err) {
-//     console.log(err)
-//   }
-// })();
+// define a secure route handler for the login page that redirects to /guitars
+app.get("/login", oidc.ensureAuthenticated(), (req, res) => {
+  res.redirect("/guitars");
+});
 
-// ! Call pool.end() on application shutdown. There is no need ot close individual clients when using a pool.
+// define a route to handle logout
+app.get("/logout", (req: any, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+// define a secure route handler for the guitars page
+app.get("/guitars", oidc.ensureAuthenticated(), (req: any, res) => {
+  res.render("guitars");
+});
 
 export default app;
