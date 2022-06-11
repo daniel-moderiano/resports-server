4ff439497dfa6717728340d3cb918c19819f29e7
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-06-11 12:15:23 +0930

    Add tests for initdb

--- src/__tests__/dbHelpers.test.ts
+++ src/__tests__/dbHelpers.test.ts
@@ -1,16 +1,7 @@
 import { selectAllFromTable, dropTable } from "../db/helpers";
-import { createTable } from "../db/testUtils";
 import './dbSetupTeardown';
 
 describe('database helper functions', () => {
-  describe('createTable', () => {
-    it('should create the table in the database', async () => {
-      const res = await createTable('items');
-      // Because the table has just been created, no rows should exist
-      expect(res.rowCount).toBeNull();
-    })
-  });
-
   describe('drop table', () => {
     it('should create the table in the database', async () => {
       // const res = await dropTable('items');
@@ -21,9 +12,8 @@ describe('database helper functions', () => {
 
   describe('select all', () => {
     it('should select items from the table', async () => {
-      const res = await selectAllFromTable('items')
+      const res = await selectAllFromTable('users')
       console.log(res);
-
       // After each db test run, the 'items' table will be dropped, so we can be sure there is only one row in the table - the row inserted in the insert test
       expect(res.rows[0]).toStrictEqual({ id: 1, name: 'steering wheel', price: '62.59' })
     })
