4f8970532fb935eca7dc64d644502e7771d5d04e
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-06-13 21:41:04 +0930

    Add test conditional for accessing res.locals user in test environments where req.oidc.user is not available

--- src/controllers/subscriptionControllers.ts
+++ src/controllers/subscriptionControllers.ts
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
-import { deleteSubscription, selectAllFromTable, selectSubscription } from '../db/helpers';
+import { deleteSubscription, insertSubscription, selectAllFromTable, selectSubscription } from '../db/helpers';
+import { body, validationResult } from 'express-validator'
 
 interface Subscription {
   subscription_id: number;
@@ -13,6 +14,16 @@ interface Subscription {
 // @route   GET /api/subscriptions/subscriptionId
 // @access  Private
 const getSubscription = asyncHandler(async (req, res) => {
+  let userId: string | undefined;
+
+  // For testing purposes, use the res.locals object, which can be changed to suit testing needs
+  if (process.env.TEST_ENV === 'true') {
+    userId = res.locals.user!.sub.split('|')[1] as string
+    console.log('Testing', userId);
+  } else {
+    userId = req.oidc.user!.sub.split('|')[1] as string;
+  }
+
   // subscription ID grabbed from URL params
   const subscriptionId = req.params.subscriptionId;
   const result = await selectSubscription(subscriptionId);
@@ -33,9 +44,43 @@ const getSubscription = asyncHandler(async (req, res) => {
 // @desc    Add new subscription
 // @route   POST /api/subscriptions
 // @access  Private
-const addSubscription = asyncHandler(async (req, res) => {
-  res.send('Add sub');
-});
+const addSubscription = [
+  // Validate input
+  body('channelId', 'Channel ID is required').trim().isString().isLength({ min: 1 }),
+  body('platform', 'Video platform is required').trim().isString().isLength({ min: 1 }),
+
+  // Process request after input data has been validated
+  asyncHandler(async (req, res, next) => {
+    // Because this route is protected, this controller will not be reached unless the user is authenticated. Therefore, we will always have access too res.oidc.user at this point. A conditional check is redundant here.
+    // Isolate the ID portion from the authentication type (e.g. auth0|1234 -> 1234)
+    let userId: undefined | string;
+
+    // For testing purposes, use the res.locals object, which can be changed to suit testing needs
+    if (process.env.TEST_ENV === 'true') {
+      userId = res.locals.user!.sub.split('|')[1] as string
+      console.log('Testing', userId);
+    } else {
+      userId = req.oidc.user!.sub.split('|')[1] as string;
+    }
+
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    // Validation errors have occurred. Return these to the user
+    if (!errors.isEmpty()) {
+      res.status(400).json(errors.array());   // Do not throw single error here, pass all validation errors
+    } else {
+
+      const newSubscription = await insertSubscription({
+        channelId: req.body.channelId,
+        userId: userId,
+        platform: req.body.platform
+      });
+
+      res.status(200).json(newSubscription);   // Return status OK and new subscription to client
+    }
+  }),
+];
 
 // @desc    Delete subscription
 // @route   DELETE /api/subscriptions/subscriptionId
