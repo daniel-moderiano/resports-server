b2059b90b6211488d6fd2e57bf56018d1e5375ab
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-07 20:24:50 +1030

    Add pg-test library and adjust jest config accordingly

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFiles: ['dotenv/config'],
-  testPathIgnorePatterns: ['dist', 'src/__tests__/dbSetupTeardown.ts']
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFiles: ["dotenv/config"],
+  testPathIgnorePatterns: ["dist", "src/__tests__/dbSetupTeardown.ts"],
+  globalSetup: "<rootDir>/node_modules/@databases/pg-test/jest/globalSetup",
+  globalTeardown:
+    "<rootDir>/node_modules/@databases/pg-test/jest/globalTeardown",
 };
