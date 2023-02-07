237b2c0bcd3174a81def3374df3387f55dd61619
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-07 11:16:10 +1030

    Define saved_channels table schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,6 +1,6 @@
--- Will link with AWS Cognito to provide cognito_username
+-- Will link with AWS Cognito to provide username
 CREATE TABLE [IF NOT EXISTS] users (
-  cognito_username TEXT UNIQUE NOT NULL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL PRIMARY KEY,
   email TEXT CI UNIQUE NOT NULL,
   created_on TIMESTAMP NOT NULL
 );
@@ -9,4 +9,14 @@ CREATE TABLE [IF NOT EXISTS] users (
 CREATE TABLE [IF NOT EXISTS] channels (
   channel_id TEXT NOT NULL PRIMARY KEY,
   platform TEXT NOT NULL
+);
+
+CREATE TABLE [IF NOT EXISTS] saved_channels (
+  username TEXT NOT NULL,
+  channel_id TEXT NOT NULL,
+  PRIMARY KEY (username, channel_id),
+  FOREIGN KEY (username)
+    REFERENCES users (username),
+  FOREIGN KEY (channel_id)
+    REFERENCES channels (channel_id)
 );
