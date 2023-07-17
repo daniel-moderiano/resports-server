e8550badb4a722e39d53ccb3153716259abda3e4
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-07-17 14:30:39 +0930

    Remove dist

--- dist/types/APITypes.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// This file aims to define types related to the auth0 API, particularly expected API responses or structured objects required when making API calls
-Object.defineProperty(exports, "__esModule", { value: true });
